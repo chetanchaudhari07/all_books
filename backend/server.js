ff8cbb5fe14d75f2d36284162d6d7d471a424902
chetanchaudhari07 <chetancdi077@gmail.com>
2025-09-20 20:51:55 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
 
-app.use(
-  cors({
-    origin: "https://all-books-zfz6.vercel.app", 
-    credentials: true,              
-  })
-);
+app.use(cors());
 
 app.use(morgan('dev'));
 
