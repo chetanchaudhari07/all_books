de79a0a942f1c899abc956cb7fc7d626d75f6a83
chetanchaudhari07 <chetancdi077@gmail.com>
2025-09-21 12:36:31 +0530

    update_CORS

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 
 app.use(cors({
-  origin: ["https://all-books-zfz6.vercel.app"],
+  origin: ["https://all-books-ashy.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
